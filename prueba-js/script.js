5419e13847a11df4cf4d31527840dbbcb6b4cd7d
Daoritox <davidortega0099@gmail.com>
2025-10-21 23:22:02 +0200

    Refactor script.js to include age validation and countdown logic

--- prueba-js/script.js
+++ prueba-js/script.js
@@ -1,3 +1,27 @@
 // Tipos de Datos
 const numero = 12 
 typeof 12 // number
+
+const edad = 16
+
+if (edad >= 18) {
+    console.log("Eres mayor de edad");
+} else if (edad >= 16) {
+    console.log("Casi eres mayor de edad");
+} 
+else {
+    console.log("Eres menor de edad");
+}
+
+// while 
+
+let cuentaAtras = 10 
+while (cuentaAtras >0) {
+    cuentaAtras = cuentaAtras -1
+    console.log(cuentaAtras + ' segundos')
+}
+
+// for 
+for (let i = 0; i < 5; i = i + 1) {
+    console.log("Iteración número " + i)
+}  

92f5d1cb3be9198d6ac20e08a0f7cb3ab869aa87
Daoritox <davidortega0099@gmail.com>
2025-10-22 23:13:53 +0200

    Add additional JavaScript functions and control structures for enhanced functionality

--- prueba-js/script.js
+++ prueba-js/script.js
@@ -24,4 +24,99 @@ while (cuentaAtras >0) {
 // for 
 for (let i = 0; i < 5; i = i + 1) {
     console.log("Iteración número " + i)
-}  
+}
+
+// do while 
+let resultado 
+do {
+    resultado =confirm("¿Deseas continuar?")
+} while (resultado)
+
+
+// variables y declaraciones
+
+let name = "Daoritox" //decalración de variable
+
+// expresiones
+2, true, false
+
+// for 
+for (let i = 1; i<=100; i = i + 1) {
+    console.log(i)
+}
+
+// que cuente del 1 al 10 
+for (let number= 1; number <=10; number++) {
+    console.log(number)
+}
+
+//switch 
+
+const dia  = 'lunes'
+switch (dia) {
+    case 'lunes': 
+        console.log("Hoy es lunes");
+        break;
+
+    default: 
+    console.log("No es lunes");
+        break;
+}
+
+const dia2 = new Date().getDay()
+
+//segun el dia de lasemana mostraremos un mensaje diferente
+
+switch (dia2) {
+    case 0: 
+    console.log("Hoy es domingo");
+    break;
+
+    case 1:
+    console.log("Hoy es lunes");
+    break;
+
+    case 2:
+    console.log("Hoy es martes");
+    break;
+
+    case 3:
+    console.log("Hoy es miércoles");
+    break;
+
+    case 4:
+    console.log("Hoy es jueves, se acerca el fin de semana");
+
+    case 5:
+    case 6: 
+    console.log("Es fin de semana");
+    break;
+}
+
+// Creando mi primera función
+function saludar() {
+    console.log("Hola, soy daoritox")
+}
+
+// para ejecutar la funcion -> saludar()
+
+function suma() {
+    return 2 + 2
+}
+
+let resultadoo = suma()
+console.log(resultado)
+
+// otra funcion porque si 
+
+function getRandomNumber() {
+    const random = Math.random() // genera un numero aleatorio entre 0 y 1
+
+    const multiplied = random * 10 // multiplica el numero aleatorio por 10
+
+    const rounded = Math.floor(multiplied)
+
+    const result = rounded + 1
+
+    return result
+}
